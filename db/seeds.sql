98cdf6fafa602f5b20a70086cf31b876842eb4d4
Alex Engelmann <alex.engelmann@gmail.com>
2019-06-20 17:38:45 -0700

    basic dir structure in place

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken bacon swiss", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Royale with cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Guacamole bacon burger", false);
+
---
+++ models/burger.js
@@ -0,0 +1,26 @@
+let orm = require("../config/orm.js");
+
+//TODO these functions are probably a bit off
+
+var burger = {
+    all: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+
+
+
+module.exports = burger;
