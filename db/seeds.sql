f59ac24d1dd31fa9ba6d42f234544b4b95b93c3c
Alex Engelmann <alex.engelmann@gmail.com>
2019-06-22 19:26:17 -0700

    eaten and uneaten burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
-
-INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken bacon swiss", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Royale with cheese", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Guacamole bacon burger", false);
-
+INSERT INTO burgers (name) VALUES ('Royale with cheese');
+INSERT INTO burgers (name) VALUES ('Quarter pounder with cheese');
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Guacamole bacon burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Santa Fe chicken burger', true);
+INSERT INTO burgers (name) VALUES ('Jalapeño burger');
